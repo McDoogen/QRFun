4e05a13e89dc9e7fbfb13cc7b1430271d97e8ab9
Douglas Smith <McDoogen@gmail.com>
2021-01-27 14:40:42 -0500

    Create initial documents

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	// Take a picture with the camera
+	
+	// Read in the image MAT object
+	
+	// Declare the bbox and rectifiedImage Mat objects
+	
+	// Create a QRCodeDetector Object
+	
+	// Detect and Decode the QR code in the image
+	
+	// Check if a QR code was detected
+	
+	// Print the decoded text
+	
+	cout << "Initialized! :D" << endl;
+	
+	return 0;
+}
